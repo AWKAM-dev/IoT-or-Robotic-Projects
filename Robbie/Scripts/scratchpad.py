682309a27e4c1e2881619ee3bc659f976d6692b0
AWKAM-dev <adityawilsonuk@gmail.com>
2025-10-12 21:51:15 +0530

    Add scratchpad into Robbie.ino and try-except
    Added scratchpad IK logic into Robbie.ino as inverseK function. Added try-except blocks for math.acos functions to catch domain errors and put 0 as default values for the angles.

--- Robbie/Scripts/scratchpad.py
+++ Robbie/Scripts/scratchpad.py
@@ -9,25 +9,35 @@ CD = 10.5
 #Triangle ABE
 BE = math.sqrt(AB**2 + AE**2)
 print(f"BE is {BE}")
-BED = math.degrees(math.acos(AE/BE))
-print(f"Angle BED is {BED}")
-ABE = 90 - BED
+BEA = math.degrees(math.acos(AE/BE))
+print(f"Angle BEA is {BEA}")
+ABE = 90 - BEA
 
 #Triangle BDE
 BED = ABE
 BD = math.sqrt(BE**2 + DE**2 - 2*BE*DE*(math.cos(math.radians(BED))))
 print(f"BD is {BD}")
-EBD = math.degrees(math.acos((DE**2 - BD**2 - BE**2)/(-2*BD*BE)))
-print(f"EBD is {EBD}")
+
+try:
+	EBD = math.degrees(math.acos((DE**2 - BD**2 - BE**2)/(-2*BD*BE)))
+	print(f"EBD is {EBD}")
+except Exception as e:
+	print(f"[ERROR]: {e} for trianglr BDE")
+	EBD = 0
 
 #Triangle FBD
 FBD = 90 - ABE - EBD
 
 #Traingle BCD
-BCD = math.degrees(math.acos((BD**2 - BC**2 - CD**2)/(-2*BC*CD)))
-print(f"BCD is {BCD}")
-CBD = math.degrees(math.acos((CD**2-BC**2-BD**2)/(-2*BC*BD)))
-print(f"CBD is {CBD}")
+try:
+	BCD = math.degrees(math.acos((BD**2 - BC**2 - CD**2)/(-2*BC*CD)))
+	print(f"BCD is {BCD}")
+	CBD = math.degrees(math.acos((CD**2-BC**2-BD**2)/(-2*BC*BD)))
+	print(f"CBD is {CBD}")
+except Exception as e:
+	print(f"[ERROR]: {e} for triangle BCD")
+	BCD = 0
+	CBD = 0
 
 #Servos
 botServo = 90 - CBD - FBD
