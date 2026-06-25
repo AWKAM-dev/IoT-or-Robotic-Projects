068c1c8e0a51e90a8cfdc36a1cc59c5737f4fbdf
AWKAM-dev <adityawilsonuk@gmail.com>
2026-06-25 12:12:24 +0530

    Finished setting up Setup checks for LIS3DH

--- Mapper_and_Drone/LIS3DH/LIS3DH.ino
+++ Mapper_and_Drone/LIS3DH/LIS3DH.ino
@@ -7,6 +7,15 @@ Adafruit_LIS3DH lis = Adafruit_LIS3DH();
 void setup() {
   Serial.begin(115200);
   while(!Serial) delay(10); //Wait for Serial monitor to load up
+
+  Serial.println("LIS3DH Accelerometer Test");
+
+  //Initialize sensor with the default I2C address (0x18). Ensure GND to SDO to lock down 0x18
+  if(!lis.begin(0x18)){
+    Serial.println("Couldn't find a valid LIS3DH sensor, check wiring!");
+    while(1) delay(10);
+  }
+  Serial.println("LIS3DH found!");
 }
 
 void loop() {
