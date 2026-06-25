a9839a7a522e103f336cd34a3830db14a071f4c9
AWKAM-dev <adityawilsonuk@gmail.com>
2026-06-25 12:18:26 +0530

    Actually finished up Setup for LIS3DH

--- Mapper_and_Drone/LIS3DH/LIS3DH.ino
+++ Mapper_and_Drone/LIS3DH/LIS3DH.ino
@@ -7,6 +7,26 @@ Adafruit_LIS3DH lis = Adafruit_LIS3DH();
 void setup() {
   Serial.begin(115200);
   while(!Serial) delay(10); //Wait for Serial monitor to load up
+
+  Serial.println("LIS3DH Accelerometer Test");
+
+  //Initialize sensor with the default I2C address (0x18). Ensure GND to SDO to lock down 0x18
+  if(!lis.begin(0x18)){
+    Serial.println("Couldn't find a valid LIS3DH sensor, check wiring!");
+    while(1) delay(10);
+  }
+  Serial.println("LIS3DH found!");
+
+  //Set sensor measurement range (LIS3DH_RANGE_2_G, LIS3DH_RANGE_4_G, LIS3DH_RANGE_8_G, LIS3DH_RANGE_16_G)
+  lis.setRange(LIS3DH_RANGE_2_G);
+
+  Serial.print("Range set to: ");
+  Serial.print(2 << lis.getRange());
+  Serial.println("G");
+
+  //Set data rate: Lower data rate = lower power consumption
+  //Options: LIS3DH_DATARATE_1_HZ - LIS3DH_DATARATE_400_HZ, or LIS3DH_DATARATE_POWERDOWN
+  lis.setDataRate(LIS3DH_DATARATE_50_HZ);
 }
 
 void loop() {
