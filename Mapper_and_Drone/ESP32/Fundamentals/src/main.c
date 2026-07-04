a221b21dc1dcee1fa38b0152bc73b345a0c1718a
AWKAM-dev <adityawilsonuk@gmail.com>
2026-07-04 22:47:56 +0530

    Created Blinky Setup Project
    Blinky setup project to identify build and upload problems (dependencies error)

--- Mapper_and_Drone/ESP32/Fundamentals/src/main.c
+++ Mapper_and_Drone/ESP32/Fundamentals/src/main.c
@@ -1,1 +1,18 @@
-void app_main() {}
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <driver/gpio.h>
+#include <freertos/task.h>
+
+#define LED_PIN 13
+
+void app_main(void){
+    esp_rom_gpio_pad_select_gpio(LED_PIN);
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+    int ON = 0;
+
+    while(true){
+        ON = !ON;
+        gpio_set_level(LED_PIN, ON);
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+    }
+}
