377dfed7f3bb63cc36cdb053ac3e54ceb7a56fb4
AWKAM-dev <adityawilsonuk@gmail.com>
2026-07-05 21:51:05 +0530

    Implemented trapezoidalRule function

--- Mapper_and_Drone/maths/Integration.c
+++ Mapper_and_Drone/maths/Integration.c
@@ -16,6 +16,23 @@ float riemannSums(float* input[]){
     return sum;
 }
 
+float trapezoidalRule(float* input[]){
+
+    float sum = 0;
+    float h = (input[sizeof(input)-1] - input[0]) / (sizeof(input) - 1);
+    float firstPoint = *input[0];
+    float lastPoint = *input[sizeof(input)-1];
+
+    for(int i = 0; i < sizeof(input); i++){
+        sum += *input[i];
+    }
+    sum = (h / 2) * (firstPoint + lastPoint + (2 * sum));
+    printf("Trapezoidal Rule: %f\n", sum);
+    return sum;
+}
+
+
+
 int main(){
 
 }
