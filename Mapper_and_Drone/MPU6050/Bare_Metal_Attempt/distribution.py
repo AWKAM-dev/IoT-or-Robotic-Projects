51b810f7c95034962dfb6d228e5932dcc4369e93
AWKAM-dev <adityawilsonuk@gmail.com>
2026-07-26 22:18:14 +0530

    Added Serial reader python program

---
+++ Mapper_and_Drone/MPU6050/Bare_Metal_Attempt/distribution.py
@@ -0,0 +1,27 @@
+import serial
+import time
+
+#Configuring Serial port
+PORT = "/dev/ttyACM0"
+BAUDRATE = 9600
+
+try:
+    with serial.Serial(PORT, BAUDRATE, timeout=1) as ser:
+        print(f"Connected to {PORT}. Waiting for data...")
+        time.sleep(2)
+
+        while True:
+            #Read a line until a newline character is hit
+            raw_data = ser.readline()
+
+            #Only process if data was actually recieved
+            if raw_data:
+                #Decode bytes to string and strip whitespaces
+                text_data = raw_data.decode("utf-8").strip()
+                print(f"Recieved {text_data}")
+
+except serial.SerialException as e:
+    print(f"Error opening serial port as {e}")
+
+except KeyboardInterrupt:
+    print("\nProgramm stopped by user.")
